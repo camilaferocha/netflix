3c413f4407fdae0122316fefc6dbadf932aca56c
camilaferocha <86987052+camilaferocha@users.noreply.github.com>
2021-10-06 20:24:11 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 function CarregarFilmes(){
     //array --> variável com subdivisões(indices)
-    let filmes = ["images/chegada.png", "images/yesterday.png", "images/escola.png", "images/parasita.jpg", "images/birdbox.jpg"]
+    let filmes = [
+        "images/chegada.png" , 
+        "images/yesterday.png" , 
+        "images/escola.png" , 
+        "images/parasita.jpg" , 
+        "images/birdbox.jpg"
+    ]
+
+    let totalFilmes = filmes.length
 
     //laço de repetição
-    for(let controle = 0 ; controle < 6 ; controle + 1 ){
-        document.querySelector(".lista-filmes").innerHTML +- filmes [controle] + ">"
-
+    for(let i = 0 ; i < totalFilmes ; i++){
+        document.querySelector(".lista-filmes").innerHTML += "<img src="+ filmes[i] + ">"
     }
 }
 
